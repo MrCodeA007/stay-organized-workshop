b6dc0f7d54a5468c612a5df4e065f3e249efcbb1
Alejandro Regus <Aregus@appdev.yearup.org>
2023-06-16 11:55:47 -0400

    table of todo for selected user set

--- todo.js
+++ todo.js
@@ -1,6 +1,10 @@
-const tableEl = document.getElementById(`userTable`);
+const tableEl = document.getElementById(`usersTable`);
 const searchUserEl = document.getElementById(`searchUser`);
-const addtodoBtnEl = document.getElementById(`addtodoBtn`);
+const toNewTodoBtnEl = document.getElementById(`toNewTodo`);
+
+toNewTodoBtnEl.addEventListener(`click`, () => {
+  window.location.href = "new_todo.html";
+})
 
 function searchUserdropdown() {
   fetch(`http://localhost:8083/api/users`)
@@ -17,14 +21,30 @@ function searchUserdropdown() {
 }
 
 searchUserEl.addEventListener(`change`, () => {
-    let selectedUser = searchUserEl.value;
+  let selectedUser = searchUserEl.value;
   const url = `http://localhost:8083/api/todos/byuser/${selectedUser}`;
+  const tbodyEl = tableEl.querySelector(`tbody`);
+  tbodyEl.innerHTML = ``
   fetch(url)
-  .then((response) => response.json())
-  .then((todos)=> {
+    .then((response) => response.json())
+    .then((todos) => {
+      //console.log(todos)
+      todos.forEach((todo) => {
+       
+        let row = tbodyEl.insertRow();
+        let categorycell = row.insertCell();
+        categorycell.textContent = todo.category
 
-  })
- 
+        let descriptioncell = row.insertCell();
+        descriptioncell.textContent = todo.description
+
+        let deadlinecell = row.insertCell();
+        deadlinecell.textContent = todo.deadline
+
+        let prioritycell = row.insertCell();
+        prioritycell.textContent = todo.priority
+      });
+    });
 });
 //----------------------------------------------------------------------
 searchUserdropdown();
